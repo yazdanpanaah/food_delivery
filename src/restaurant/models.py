f938d928638b2a2474b0f18280343dbd41e2010d
yazdanpanaah <samyz1024@gmail.com>
2021-12-27 17:26:55 +0330

    edit tables in restaurant models

--- src/restaurant/models.py
+++ src/restaurant/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.validators import (MinValueValidator, MaxValueValidator, MinLengthValidator)
 from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
+from account.models import *
 
 # Create your models here.
 
@@ -13,24 +15,16 @@ class Restaurant(models.Model):
         abstract = True
 
 
-class Department(models,Model):
-    name =models.CharField(max_length=100)
+class Department(Restaurant):
     description = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     created_date = models.DateTimeField(auto_now_add=True)
-    resturant_id = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
     manager_id = models.OneToOneField('Manager',on_delete=models.CASCADE)
-    order_id = models.ForeignKey("Order",on_delete=models.CASCADE)
-
+    category_id=models.OneToOneField("Category",on_delete=models.CASCADE)
     def __str__(self) -> str:
         return self.name
 
-class DepartmentCategory(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return self.name
 
 
 class Food(models.Model):
@@ -39,15 +33,21 @@ class Food(models.Model):
     discreption=models.TextField(max_length=200)
     created_date=models.DateTimeField(auto_add_now=True)
     category_id=models.ForeignKey("Category",on_delete=models.CASCADE)
-    # cart_id=models.ManyToManyField("Cart")
     menu_id=models.ManyToManyField('Menu',through='Food_Menu')
 
     def __str__(self) -> str:
         return self.name
 
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        self.category
+
 
 class Menu(models.Model):
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f'{self.department_id} Menu'
 
 class Food_Menu(models.Model):
     Food_id = models.ForeignKey(Food, on_delete= models.CASCADE)
@@ -55,12 +55,33 @@ class Food_Menu(models.Model):
     price = models.IntegerField()
     number = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f'{self.Menu_id}'
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=100)
     food_id = models.ManyToManyField(Food)
-
     def __str__(self) -> str:
         return self.name
 
+class Order(models.Model):
+    ORDER_STATUS = (
+        ('order', 'order'),
+        ('order_registration', 'order_registration'),
+        ('Send', 'Send'),
+        ('Delivery', 'Delivery'),
+        
+    )
+    number = models.IntegerField()
+    total_price = models.IntegerField()
+    delivery_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices= ORDER_STATUS, default='order_registration')
+    created_date = models.DateTimeField(auto_now_add=True)
+    CAraddress_id=models.OneToOneField(Adress,on_delete=models.CASCADE)
+    department_id = models.ForeignKey(Department ,on_delete=CASCADE)
+
+    def __str__(self) :
+        return f'{self.department_id} order'
 
 
